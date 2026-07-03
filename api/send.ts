c478980b3268b37d3c98a94bf057371c7224fd8d
Alex-sandro1 <mgonjakitaraju@gmail.com>
2026-07-03 20:03:31 +0300

    for

--- api/send.ts
+++ api/send.ts
@@ -5,11 +5,10 @@ import mongoose from 'mongoose'
 import {thystatus} from '../thyschema'
 import {thyconny} from '../thyconn'
 
-let thycache:typeof mongoose|null=null
-await thyconny(thycache)
-const thymod=mongoose.model("thystatus", thystatus)
-
 export default async function thysend(req:any,res:any){
+    let thycache:typeof mongoose|null=null
+    await thyconny(thycache)
+    const thymod=mongoose.model("thystatus", thystatus)
     const thysat=await thymod.find({})
     const thyy=await countilize(thysat,thyref)
     res.json(thyy)
