738aa501081ab66baae80ceef8a8d2ba0bd5acb0
Alex-sandro1 <mgonjakitaraju@gmail.com>
2026-07-03 20:00:03 +0300

    another change a

--- api/send.ts
+++ api/send.ts
@@ -9,9 +9,8 @@ let thycache:typeof mongoose|null=null
 await thyconny(thycache)
 const thymod=mongoose.model("thystatus", thystatus)
 
-const svr=express()
 export default async function thysend(req:any,res:any){
     const thysat=await thymod.find({})
-    const thyy=countilize(thysat,thyref)
+    const thyy=await countilize(thysat,thyref)
     res.json(thyy)
 }
