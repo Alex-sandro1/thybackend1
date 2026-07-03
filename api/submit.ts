35da1b616713e76bee52e6db7db25743276f0b46
Alex-sandro1 <mgonjakitaraju@gmail.com>
2026-07-03 19:33:37 +0300

    thyy

--- api/submit.ts
+++ api/submit.ts
@@ -6,16 +6,10 @@ import bodyParser from 'body-parser'
 import {countilize} from '../countilise'
 import {thyref} from '../thyref'
 
-const svr=express()
-svr.use(bodyParser.json())
-mongoose.connect("mongodb://alesandro1:JsIEpzWnvdiwNEgJ@ac-ppjwoqd-shard-00-00.pjfjxpf.mongodb.net:27017,ac-ppjwoqd-shard-00-01.pjfjxpf.mongodb.net:27017,ac-ppjwoqd-shard-00-02.pjfjxpf.mongodb.net:27017/?ssl=true&replicaSet=atlas-e2inq3-shard-0&authSource=admin&appName=Cluster0"
-    ,{serverSelectionTimeoutMS:6000,
-        socketTimeoutMS:6000
-    }
-)
-.then(()=>{console.log("N1gga suceeees!")})
 let thycache:typeof mongoose|null=null
 await thyconny(thycache)
+const svr=express()
+svr.use(bodyParser.json())
 const thyresult=mongoose.model("thystatus", thystatus)
 //svr.use(express.json())
 export default async function thyhandle(req:any,res:any){
