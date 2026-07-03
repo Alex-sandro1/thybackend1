5fb88af728e17225628051699e2e1b75ada1dc42
Alex-sandro1 <mgonjakitaraju@gmail.com>
2026-07-03 19:37:28 +0300

    thy1

--- api/send.ts
+++ api/send.ts
@@ -3,9 +3,10 @@ import {thyref} from '../thyref'
 import {countilize} from '../countilise'
 import mongoose from 'mongoose'
 import {thystatus} from '../thyschema'
+import {thyconny} from '../thyconny'
 
-mongoose.connect("mongodb://alesandro1:JsIEpzWnvdiwNEgJ@ac-ppjwoqd-shard-00-00.pjfjxpf.mongodb.net:27017,ac-ppjwoqd-shard-00-01.pjfjxpf.mongodb.net:27017,ac-ppjwoqd-shard-00-02.pjfjxpf.mongodb.net:27017/?ssl=true&replicaSet=atlas-e2inq3-shard-0&authSource=admin&appName=Cluster0")
-.then(()=>{console.log("Countilize dAMN connected")})
+let thycache:typeof mongoose|null=null
+await thyconny(thycache)
 const thymod=mongoose.model("thystatus", thystatus)
 
 const svr=express()
@@ -13,6 +14,5 @@ const svr=express()
 export default async function thysend(req:any,res:any){
     const thysat=await thymod.find({})
     const thyy=countilize(thysat,thyref)
-    console.log(thyy)
     res.json(thyy)
 }
