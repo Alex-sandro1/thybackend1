d7e5b287f8e1d467f07858e0f4a573d5b33adc76
Alex-sandro1 <mgonjakitaraju@gmail.com>
2026-07-03 16:36:31 +0300

    critcal change

--- api/send.ts
+++ api/send.ts
@@ -4,7 +4,7 @@ import {countilize} from '../countilise'
 import mongoose from 'mongoose'
 import {thystatus} from '../thyschema'
 
-mongoose.connect("mongodb://alesandro1:<db_password>@ac-ppjwoqd-shard-00-00.pjfjxpf.mongodb.net:27017,ac-ppjwoqd-shard-00-01.pjfjxpf.mongodb.net:27017,ac-ppjwoqd-shard-00-02.pjfjxpf.mongodb.net:27017/?ssl=true&replicaSet=atlas-e2inq3-shard-0&authSource=admin&appName=Cluster0")
+mongoose.connect("mongodb://alesandro1:JsIEpzWnvdiwNEgJ@ac-ppjwoqd-shard-00-00.pjfjxpf.mongodb.net:27017,ac-ppjwoqd-shard-00-01.pjfjxpf.mongodb.net:27017,ac-ppjwoqd-shard-00-02.pjfjxpf.mongodb.net:27017/?ssl=true&replicaSet=atlas-e2inq3-shard-0&authSource=admin&appName=Cluster0")
 .then(()=>{console.log("Countilize dAMN connected")})
 const thymod=mongoose.model("thystatus", thystatus)
 
